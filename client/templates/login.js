1e2568fb08156de0d1662c8fb53aa8df5c7a75de
EdwardOkech <okechjobs@gmail.com>
2016-01-13 14:37:29 +0300

    Added template helper methods for email validation

--- client/templates/login.js
+++ client/templates/login.js
@@ -1,37 +1,62 @@
+
+//Helper method to help in form validation
+Template.login.helpers({
+
+    trimInputHelper : function(val){
+        return val.replace(/^\s*|\s*$/g, ""); // regex to remove white space in email field
+    }
+});
+
+
 Template.login.events({
-    'submit .registerform': function(event){
+    'submit #registerform': function(event, template){
         event.preventDefault();
 
-        var first_name = event.target.first_name.value;
-        var last_name = event.target.last_name.value;
-        var email = event.target.email.value;
-        var password = event.target.password.value;
+        var first_name = template.find('#first_name').value,
+            last_name = template.find('#last_name').value,
+            email = template.find('#reg-email').value,
+            password = template.find('reg-password').value,
+            password1 = template.find('reg-password1').value;
 
-        var user = {'email':email, password:password, profile: {name: first_name +" "+last_name}};
+        email = trimInputHelper(email);
+        var val_err = 'Passwords must match';
+        if(password != password1){
+            return val_err;
+        }
+
+        var user = {'email':email, 'password':password, 'profile': {'name': first_name +" "+last_name}};
 
         Accounts.createUser(user, function(err){
             if(!err){
-                Router.go('/');
+
+                //Router.go('/');
+                /*using has no effect on the page because
+                 meteor recognizes we are on the same page and doesn't refresh
+                 so opted to use pure javasript
+                  */
+                document.location.reload(true);
             }
         });
+        return false; // return false to prevent form submission from reloading the page
     }
 });
 
 
 
 Template.login.events({
-    'submit .loginform': function(event){
+    'submit #loginform': function(event, t){
         event.preventDefault();
 
-        var email = event.target.email.value;
-        var password = event.target.password.value;
+        var email = event.find('#login-email').value,
+         password = event.find('#login-password').value;
 
-        Meteor.loginWithPassword(username, email, function(err) {
+        Meteor.loginWithPassword(email, password, function(err) {
             if(!err){
-                Router.go('/');
+                //Router.go('/');
+                document.location.reload(true);
             }
         });
-
+        return false;
 
 
     }
