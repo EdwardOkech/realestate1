fab1e4726c56ccfb7efa0f8f216e80945ea8a500
sft <okechjobs@gmail.com>
2016-01-13 20:08:29 +0300

    Added ids to form imputs

--- client/templates/login.js
+++ client/templates/login.js
@@ -1,1 +1,38 @@
+Template.login.events({
+    'submit .registerform': function(event){
+        event.preventDefault();
 
+        var first_name = event.target.first_name.value;
+        var last_name = event.target.last_name.value;
+        var email = event.target.email.value;
+        var password = event.target.password.value;
+
+        var user = {'email':email, password:password, profile: {name: first_name +" "+last_name}};
+
+        Accounts.createUser(user, function(err){
+            if(!err){
+                Router.go('/');
+            }
+        });
+    }
+});
+
+
+
+Template.login.events({
+    'submit .loginform': function(event){
+        event.preventDefault();
+
+        var email = event.target.email.value;
+        var password = event.target.password.value;
+
+        Meteor.loginWithPassword(username, email, function(err) {
+            if(!err){
+                Router.go('/');
+            }
+        });
+
+
+
+    }
+});
