8993cd46b9540e67c19427cfcf2072331400283a
EdwardOkech <okechjobs@gmail.com>
2016-01-15 13:25:44 +0300

    Added more templates and helpers for the theme layout

--- client/templates/login.js
+++ client/templates/login.js
@@ -8,53 +8,26 @@ Template.login.helpers({
 });
 
 
-Template.login.events({
-    'submit #registerform': function(event, template){
-        event.preventDefault();
 
-        var first_name = template.find('#first_name').value,
-            last_name = template.find('#last_name').value,
-            email = template.find('#reg-email').value,
-            password = template.find('reg-password').value,
-            password1 = template.find('reg-password1').value;
-
-        email = trimInputHelper(email);
-        var val_err = 'Passwords must match';
-        if(password != password1){
-            return val_err;
-        }
-
-        var user = {'email':email, 'password':password, 'profile': {'name': first_name +" "+last_name}};
-
-        Accounts.createUser(user, function(err){
-            if(!err){
-
-                //Router.go('/');
-                /*using has no effect on the page because
-                 meteor recognizes we are on the same page and doesn't refresh
-                 so opted to use pure javasript
-                  */
-                document.location.reload(true);
-            }
-        });
-        return false; // return false to prevent form submission from reloading the page
-    }
-});
 
 
 
 Template.login.events({
-    'submit #loginform': function(event, t){
+    'submit #loginform': function(event, temp){
         event.preventDefault();
 
         var email = event.find('#login-email').value,
          password = event.find('#login-password').value;
 
         Meteor.loginWithPassword(email, password, function(err) {
-            if(!err){
-                //Router.go('/');
-                document.location.reload(true);
+            if(Meteor.user()){
+                Router.go('/');
+                //document.location.reload(true);
+            } else {
+                var message = "There was an error login in: <strong>" + err.reason
+                temp.find('#form-messages').html(message)
             }
+            return
         });
         return false;
 
