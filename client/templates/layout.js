c4c87a8434d65d27a53d5f285801dfe26161474f
EdwardOkech <okechjobs@gmail.com>
2016-01-28 10:29:15 +0300

    Fixed landing page autocompletion problem..

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -5,3 +5,22 @@ Template.layoutLanding.helpers({
 	       });
     }
 });
+
+Template.layoutLanding.onRendered({
+    landingAuto : function(){
+        AutoCompletion.init('input#landing-entry');
+    }
+
+});
+
+Template.layoutLanding.events({
+    'keyup input#landing-entry' : function(){
+        AutoCompletion.autocomplete({
+            element: 'input#landing-entry',
+            collection: Cities,
+            field: 'name',
+            limit: 0,
+            sort: {name: 1}
+        });
+    }
+})
