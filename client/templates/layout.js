a1a21c10cc5a578753fb9230415fde0ca316fc8a
EdwardOkech <okechjobs@gmail.com>
2016-01-29 04:23:11 +0300

    Removed grey area before upload pic

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,7 +1,21 @@
-Template.layoutLanding.helpers({
-    onClickHelper1:function(){
-         $('#select').click(function () {
-		   $('#triangle_down').show();
-	       });
-    }
-});
+//Template.layoutLanding.helpers({
+//    onClickHelper1:function(){
+//         $('#select').click(function () {
+//		   $('#triangle_down').show();
+//	       });
+//    }
+//});
+
+//Template.layoutLanding.helpers({
+//	cityHelper: function(){
+//		return {
+//			position: "top",
+//			limit: 10,
+//			rules:[
+//			     token : '@',
+//			     collection : Meteor.cities,
+//			     field : "name"
+//			]
+//		}
+//	}
+//})
