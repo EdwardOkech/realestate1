efebf575e3f7d17056cdc764a4db229fe6538f02
EdwardOkech <okechjobs@gmail.com>
2016-02-02 04:20:26 +0300

    refactored sendMail factor

--- server/emails.js
+++ server/emails.js
@@ -1,3 +1,7 @@
+Meteor.startup(function(){
+    process.env.MAIL_URL = 'smtp://info@blomit.co.za:A4mJluKTZKXIaXob_BcRrg@smtp.mandrillapp.com:587'
+});
+
 Meteor.methods({
     sendEmail: function(userId, email){
         if(this.userId == userId){
