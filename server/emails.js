fcef4ef0a6bd7d785b7ab72c1b3f0479f3c8c2fe
EdwardOkech <okechjobs@gmail.com>
2016-02-02 03:10:16 +0300

    Email sending methods

--- server/emails.js
+++ server/emails.js
@@ -1,1 +1,14 @@
+Meteor.methods({
+    sendEmail: function(to, from, subject, text){
+        check([to, from, subject, text]);
 
+        this.unblock();
+
+        Email.send({
+            to:to,
+            from: from,
+            subject: subject,
+            text: text
+        });
+    }
+});
