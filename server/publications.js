afc636ed2aa16351d2edde3fa38b64c053e835fe
EdwardOkech <okechjobs@gmail.com>
2016-02-06 21:46:13 +0300

    Added more publications

--- server/publications.js
+++ server/publications.js
@@ -2,9 +2,26 @@ Meteor.publish('cities', function(){
     return Cities.find();
 });
 
-Meteor.publish('properties', function(){
-    return Properties.find({_id:propertyId});
+
+//Grid view properties - front page
+Meteor.publish('grid-view', function(){
+    return Properties.find({}, {
+        limit: 8,
+    });
 });
+
+Meteor.publish('acheter', function(acheter){
+    return Properties.find({acheter:acheter})
+});
+
+Meteor.publish('louer', function(louer){
+    return Properties.find({louer:louer})
+});
+
+Meteor.publish('single-property', function(_id){
+    return Properties.find({_id:_id})
+});
+
 
 Meteor.publish('tweets', function(){
     return Tweets.find();
